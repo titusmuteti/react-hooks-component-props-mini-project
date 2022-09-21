046635f32067c9787066720925db132c55e0e868
Titus Mumo <titusmumo@gmail.com>
2022-09-21 11:41:51 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import Article from "./Article"
+
+function ArticleList({posts}) {
+    return (
+    <main>
+       {posts.map((post) =>{
+        return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+       })}
+    </main>
+    )
+}
+
+export default ArticleList
